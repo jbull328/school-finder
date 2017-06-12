d4dbc8379155202bb49d22c64eca0b3a8412f970
John <jackbull328@gmail.com>
2017-06-11 21:47:26 -0700

    got schoolInfo and SchoolList working

---
+++ app/main.jsx
@@ -0,0 +1,14 @@
+var React = require("react");
+var ReactDom = require("react-dom");
+var SchoolsList = require("./components/SchoolList.jsx");
+
+var _schools = [
+  {name: "Lovedate", tagline: "This is a wonderful school"},
+  {name: "Bishop", tagline: "Another great school"}
+];
+
+function render() {
+  ReactDom.render(<SchoolsList schools = {_schools} />,
+  document.getElementById("container"))
+}
+render();
