a357f2215e6358cc31d4036dbf8b60cd4b96b59b
John <jackbull328@gmail.com>
2017-06-15 21:53:10 -0700

    added mongoose scheema and express api controller

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
 var express = require("express"),
-      path = require("path");
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    path = require("path");
+
+var schoolController = require("./controllers/schoolController");
 
 var app = express();
 app.use(express.static(path.join(__dirname,"../app/dist")));
+app.use(bodyParser.json())
+app.use("/api", schoolController);
+
 app.listen(7777, function() {
   console.log("Started School-Finder Server on port", 7777);
 })
+
+mongoose.connect("mongodb://localhost/schoolfinder");
